bafb69241ee15a7a579b6c6fbf1cd3ee1edef566
yunus <yunusdurnagol@gmail.com>
2017-07-09 22:22:36 +0300

    Customizing User Class

--- IdentityFromZero/Controllers/HomeController.cs
+++ IdentityFromZero/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using IdentityFromZero.Models;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 
@@ -14,9 +15,10 @@ namespace IdentityFromZero.Controllers
         // GET: Home
         public async Task<ActionResult> Index()
         {
-            var context =new IdentityDbContext<IdentityUser>();
-            var store=new UserStore<IdentityUser>(context);
-            var manager=new UserManager<IdentityUser>(store);
+            //var context =new IdentityDbContext<IdentityUser>();
+            var context = new ApplicationDbContext();
+            var store =new UserStore<CustomUser>(context);
+            var manager=new UserManager<CustomUser>(store);
 
 
             var email = "identity@identity.com";
@@ -24,13 +26,21 @@ namespace IdentityFromZero.Controllers
             var user = await manager.FindByEmailAsync(email);
             if (user == null)
             {
-                user = new IdentityUser()
+                user = new CustomUser()
                 {
                     UserName = email,
-                    Email = email
+                    Email = email,
+                    FirstName = "FirstName",
+                    LastName = "LastName"
                 };
 
                 await manager.CreateAsync(user, password);
+            }
+            else
+            {
+                user.FirstName = "FirstName";
+                user.LastName = "LastName";
+                await manager.UpdateAsync(user);
             }
 
 
---
+++ IdentityFromZero/Models/IdentityModels.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace IdentityFromZero.Models
+{
+    public class ApplicationDbContext:IdentityDbContext<CustomUser>
+    {
+
+        public ApplicationDbContext():base()
+        {
+                
+        }
+
+
+       
+    }
+    public class CustomUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
