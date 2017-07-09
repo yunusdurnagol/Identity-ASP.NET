199c30a0653db7273842a4026de15acd3c3abdc2
yunus <yunusdurnagol@gmail.com>
2017-07-09 21:41:39 +0300

    Necessary Objects Created

---
+++ IdentityFromZero/App_Start/IdentityConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace IdentityFromZero.App_Start
+{
+    public class IdentityConfig
+    {
+        //IUser :Represent Individual user 
+        //IUserStore : Where we save user data it is going to use EF Database Context object.
+        //UserManager<> :For CRUD operations UserManager uses UserStore
+        //SignInManager<> : In order to sign in a user it works with user to authenticate .
+
+        //IdentityUser
+        //UserStore<IdentityUser>: whenever we use user store we have to give DBContext to use
+        //IdentityDbContext<IdentityUser>
+
+    }
+}
--- IdentityFromZero/Controllers/HomeController.cs
+++ IdentityFromZero/Controllers/HomeController.cs
@@ -1,16 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace IdentityFromZero.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Home
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
+            var context =new IdentityDbContext<IdentityUser>();
+            var store=new UserStore<IdentityUser>(context);
+            var manager=new UserManager<IdentityUser>(store);
+
+
+            var email = "identity@identity.com";
+            var password = "Password1234";
+            var user = await manager.FindByEmailAsync(email);
+            if (user == null)
+            {
+                user = new IdentityUser()
+                {
+                    UserName = email,
+                    Email = email
+                };
+
+                await manager.CreateAsync(user, password);
+            }
+
+
             return Content("Hello You are on home page");
         }
     }
