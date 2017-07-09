245cfae0f372a11edbd2956c8eb81f8369723d80
yunus <yunusdurnagol@gmail.com>
2017-07-09 20:36:38 +0300

    Add StartUp and HomeController

---
+++ IdentityFromZero/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[assembly:OwinStartupAttribute(typeof(IdentityFromZero.Startup))]
+namespace IdentityFromZero
+{
+    public class Startup
+    {
+        //That is where we configure identity we would fill it later
+        public void Configuration(IAppBuilder app)
+        {
+
+        }
+    }
+}
